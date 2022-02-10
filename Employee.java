9bd8c90ff6214315180a62c21042daf43f2a0fc2
Hrishikesh Pawar <99355295+Hrishikeshpwr@users.noreply.github.com>
2022-02-10 14:57:30 +0530

    employee file

---
+++ Employee.java
@@ -0,0 +1,47 @@
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private double salary;
+	
+	public Employee(int a,String nm,double sal)
+	{
+		id=a;
+		name=nm;
+		salary=sal;
+		//constructor created
+	}
+	
+	public void showemps()
+	{
+		System.out.println("Name :"+name);
+		System.out.println("ID :"+id);
+		System.out.println("Salary :"+salary);
+	
+	}
+	
+	public String getName()
+	{
+		
+		return name;
+	}
+	
+	public String getName1()
+	{
+		if(salary>20000)
+			//System.out.print();
+			return "Name :"+name;
+		return "  ";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
