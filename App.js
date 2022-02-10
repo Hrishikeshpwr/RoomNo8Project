d89116bcd1d07e30d62b551a28c5e95d9c63b4f2
Tushar Rajabhau Jagtap <99384364+tusharrjagtap@users.noreply.github.com>
2022-02-10 15:05:29 +0530

    employee react frontend
    react frontend  from tushar

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "./component/Header";
+import LoginForm from "./component/LoginForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from "./component/Footer";
+import AddCustomer from "./component/AddCustomer";
+import AddProduct from "./component/AddProduct";
+
+import "./App.css"
+import {BrowserRouter as Router,Route,Switch}from 'react-router-dom';
+
+export default function App() {
+  return (
+    
+      <Router>
+      <Header/>
+    
+        <div className="container">
+               <Switch>
+               <Route path="/" exact component={LoginForm}></Route>
+                <Route path="/AddCustomer" component={AddCustomer}></Route>
+                <Route path="/AddProduct" component={AddProduct}></Route> 
+               </Switch>
+        </div>
+        <Footer/>
+       
+      </Router>   
+        
+  );
+}
+
